aa222af5307139355b54c3fb497623b7416250a9
eab14 <e.a.boileau14@gmail.com>
2024-03-14 00:36:55 -0400

    Minimize, Maximize, Close example on stationary windows, figuring out the css for taskbar implementation >.>

--- assets/js/window.js
+++ assets/js/window.js
@@ -1,14 +1,29 @@
 const addHandlersWindow = () => {
 
     const minimizeButton = document.querySelectorAll('.window_utilities .min');
+    const closeButton = document.querySelectorAll('.window_utilities .close');
+
+    const maximizeButton = document.querySelectorAll('.window_utilities .max');
+
+    for (let i = 0; i < minimizeButton.length; i++) maximizeButton[i].style.pointerEvents = "none";
 
     for (let i = 0; i < minimizeButton.length; i++) {
 
         minimizeButton[i].addEventListener("click", function() {
 
             let input = this.parentNode.parentNode.parentNode;
+            minimizeWindow(input, input.offsetWidth);
 
-            minimizeWindow(input, input.offsetWidth, input.offsetHeight);
+        });
+
+    }
+
+    for (let i = 0; i < closeButton.length; i++) {
+
+        closeButton[i].addEventListener("click", function() {
+
+            let input = this.parentNode.parentNode.parentNode;
+            input.style.display = "none";
 
         });
 
@@ -16,15 +31,44 @@ const addHandlersWindow = () => {
 
 }
 
-function minimizeWindow(input, initial_width, initial_height) {
+const minimizeWindow = (input, initial_width) => {
 
     let content = input.querySelector(".window_content");
-    content.style.opacity = 0;
-    content.style.height = 0;
+    let minimizeButton = input.querySelector(".min");
+    let maximizeButton = input.querySelector(".max");
 
-    gsap.to(input, 0.3, { width: 300 });
+    gsap.to(input, 0.2, { width: 300 });
+    gsap.to(content, 0.2,  { opacity: 0, height: 0, transformOrigin: "50% 50%" });
 
-    input.removeEventListener("click");
+    minimizeButton.style.pointerEvents = "none";
+
+    setTimeout(() => {
+
+        maximizeButton.style.pointerEvents = "all";
+
+        maximizeButton.addEventListener("click", function() {
+
+            maximizeWindow(input, initial_width);
+    
+        });
+        
+    }, 200);
+
+
+}
+
+const maximizeWindow = (input, initial_width) => {
+
+    let content = input.querySelector(".window_content");
+    let minimizeButton = input.querySelector(".min");
+    let maximizeButton = input.querySelector(".max");
+
+    gsap.to(input, 0.2, { width: initial_width });
+    gsap.to(content, 0.2,  { opacity: 1, height: "fit-content", transformOrigin: "50% 50%" });
+
+    maximizeButton.style.pointerEvents = "none";
+
+    setTimeout(() => minimizeButton.style.pointerEvents = "all", 200);
 
 }
 

eb696e828dfed37a2df94ac5a1878443e8afc382
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-22 21:29:50 -0400

    Remove api key

--- server/routes/weatherRoutes.js
+++ server/routes/weatherRoutes.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
+
 const router = require('express').Router()
 router.route("/").get(async (req, res) => {
+  console.log(process.env.OPENWEATHER_KEY)
   const response = await fetch(
   `https://api.openweathermap.org/data/2.5/weather?lat=${
     req.query.lat
   }&lon=${
     req.query.long
-  }&appid=${"e9dc30a76caa67c782fb10c3ba6c49a1"}`
+  }&appid=${process.env.OPENWEATHER_KEY}`
 )
 const weather = await response.json();
 res.json(weather);
